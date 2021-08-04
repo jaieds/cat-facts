a552c0bde8e82d4751338ad29f1180b22bf4f8ff
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2021-08-04 12:23:11 +0600

    Optimize images and fixed errors

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const catsImgURL = 'https://thatcopy.pw/catapi/rest/';
-const catsFactsURL = 'https://cat-fact.herokuapp.com/facts/random';
-
 const proImage = document.getElementById('pro-img');
 const factsList = document.getElementById('fact');
 const nextButton = document.getElementById('next');
 
+const catsFactsURL = `https://catfact.ninja/fact`;
+const imagesArray = [
+  '0.jpg',
+  '1.jpg',
+  '2.jpg',
+  '3.jpg',
+  '4.jpg',
+  '5.jpg',
+  '6.jpg',
+  '7.jpg',
+  '8.jpg',
+  '9.jpg',
+  '10.jpg',
+  '11.jpg',
+];
+
 async function setCatImage() {
   try {
-    const data = await fetch(catsImgURL);
-    const imgURL = await data.json();
-    proImage.style.backgroundImage = `url("${imgURL.url}")`;
+    const rndIndx = Math.floor(Math.random() * 10 + 2);
+    console.log(rndIndx);
+    proImage.style.backgroundImage = `url("/images/${imagesArray[rndIndx]}")`;
   } catch (e) {
     console.log(e);
   }
@@ -20,7 +33,7 @@ async function setCatFacts() {
     const data = await fetch(catsFactsURL);
     const fact = await data.json();
     const listElement = document.createElement('p');
-    listElement.innerText = fact.text;
+    listElement.innerText = fact.fact;
     factsList.appendChild(listElement);
   } catch (e) {
     console.log(e);
